3c5db39ff4c0420acc658920fa9a198c43f37494
Atakku <atakkudev@gmail.com>
2025-10-21 23:42:11 +0200

    fix

--- Content.Shared/Atmos/Components/PipeAppearanceComponent.cs
+++ Content.Shared/Atmos/Components/PipeAppearanceComponent.cs
@@ -10,7 +10,7 @@ public sealed partial class PipeAppearanceComponent : Component
         new(new("Structures/Piping/Atmospherics/pipe_alt1.rsi"), "pipeConnector"),
         new(new("Structures/Piping/Atmospherics/pipe_alt2.rsi"), "pipeConnector"),
         // Carpmosia-start - 5 pipe layers
-        new(new("Structures/Piping/Atmospherics/pipe_alt3.rsi"), "pipeConnector"),
-        new(new("Structures/Piping/Atmospherics/pipe_alt4.rsi"), "pipeConnector")];
+        new(new("_Carpmosia/Structures/Piping/Atmospherics/pipe_alt3.rsi"), "pipeConnector"),
+        new(new("_Carpmosia/Structures/Piping/Atmospherics/pipe_alt4.rsi"), "pipeConnector")];
         // Carpmosia-end - 5 pipe layers
 }
