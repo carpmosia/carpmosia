16bb262082aed9b2bbd4b3bb2888778d4bb4034e
Atakku <atakkudev@gmail.com>
2025-10-21 23:26:36 +0200

    Implement 5 pipe layers

--- Content.Shared/Atmos/Components/PipeAppearanceComponent.cs
+++ Content.Shared/Atmos/Components/PipeAppearanceComponent.cs
@@ -8,5 +8,9 @@ public sealed partial class PipeAppearanceComponent : Component
     [DataField]
     public SpriteSpecifier.Rsi[] Sprite = [new(new("Structures/Piping/Atmospherics/pipe.rsi"), "pipeConnector"),
         new(new("Structures/Piping/Atmospherics/pipe_alt1.rsi"), "pipeConnector"),
-        new(new("Structures/Piping/Atmospherics/pipe_alt2.rsi"), "pipeConnector")];
+        new(new("Structures/Piping/Atmospherics/pipe_alt2.rsi"), "pipeConnector"),
+        // Carpmosia-start - 5 pipe layers
+        new(new("Structures/Piping/Atmospherics/pipe_alt3.rsi"), "pipeConnector"),
+        new(new("Structures/Piping/Atmospherics/pipe_alt4.rsi"), "pipeConnector")];
+        // Carpmosia-end - 5 pipe layers
 }
