d1192f73b3f081f74b66c72ab09c97fe088aa1db
Atakku <atakkudev@gmail.com>
2025-11-10 16:31:51 +0100

    Pretty much rewritten the entire vent critter logic lol

--- Content.Server/StationEvents/Events/VentCrittersRule.cs
+++ Content.Server/StationEvents/Events/VentCrittersRule.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic; // Carpmosia-edit - Vent critter fix
 using System.Linq; // Carpmosia-edit - Vent critter fix
+using Content.Server.Pinpointer; // Carpmosia-edit - Vent critter fix
 using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
 using Content.Shared.Station.Components;
 using Content.Shared.Storage;
 using Robust.Shared.Map;
 using Robust.Shared.Random;
+using Robust.Shared.Utility; // Carpmosia-edit - Vent critter fix
 
 namespace Content.Server.StationEvents.Events;
 
@@ -15,60 +18,73 @@ public sealed class VentCrittersRule : StationEventSystem<VentCrittersRuleCompon
      * USE THE PROTOTYPE.
      */
 
-    protected override void Started(EntityUid uid, VentCrittersRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
-    {
-        base.Started(uid, component, gameRule, args);
+    // Carpmosia-start - Vent critter fix
+    [Dependency] private readonly NavMapSystem _navMap = default!;
 
-        if (!TryGetRandomStation(out var station))
-        {
+    protected override void Added(EntityUid uid, VentCrittersRuleComponent component, GameRuleComponent gameRule, GameRuleAddedEvent args)
+    {
+        PickLocation(component);
+
+        if (!TryComp<StationEventComponent>(uid, out var stationEvent))
             return;
-        }
+
+        var str = Loc.GetString("station-event-vent-creatures-start-announcement-carpmosia",
+            ("location", FormattedMessage.RemoveMarkupOrThrow(_navMap.GetNearestBeaconString((uid, component.Center)))));
+        stationEvent.StartAnnouncement = str;
+
+        base.Added(uid, component, gameRule, args);
+    }
+
+    private void PickLocation(VentCrittersRuleComponent component)
+    {
+        if (!TryGetRandomStation(out var station))
+            return;
 
         var locations = EntityQueryEnumerator<VentCritterSpawnLocationComponent, TransformComponent>();
-        var validLocations = new List<EntityCoordinates>();
+        var validLocations = new List<TransformComponent>();
         while (locations.MoveNext(out _, out _, out var transform))
         {
             if (CompOrNull<StationMemberComponent>(transform.GridUid)?.Station == station)
             {
-                validLocations.Add(transform.Coordinates);
-                // Carpmosia-start - Vent critter fix
-                // foreach (var spawn in EntitySpawnCollection.GetSpawns(component.Entries, RobustRandom))
-                // {
-                //     Spawn(spawn, transform.Coordinates);
-                // }
-                // Carpmosia-end - Vent critter fix
+                validLocations.Add(transform);
             }
         }
 
-        // Carpmosia-start - Vent critter fix
         var spawnCenter = RobustRandom.Pick(validLocations);
-        var spawnLocations = validLocations.OrderBy(c => (spawnCenter.Position - c.Position).Length()).Take(5).ToList();
+        var spawns = new List<string>();
+
+        // Guaranteed spawn
+        if (component.SpecialEntries.Count > 0)
+        {
+            var special = RobustRandom.Pick(component.SpecialEntries).PrototypeId;
+            if (special is not null)
+                spawns.Add(special);
+        }
+
         // Emulate original behaviour by trying to spawn per every valid location
         for (var i = 0; i < validLocations.Count; i++)
         {
-            foreach (var spawn in EntitySpawnCollection.GetSpawns(component.Entries, RobustRandom))
-            {
-                Spawn(spawn, spawnLocations[i % spawnLocations.Count]);
-            }
+            spawns.AddRange(EntitySpawnCollection.GetSpawns(component.Entries, RobustRandom));
+            if (component.SpecialEntries.Count > 0)
+                spawns.AddRange(EntitySpawnCollection.GetSpawns(component.SpecialEntries, RobustRandom));
         }
-        // Carpmosia-end - Vent critter fix
 
-        if (component.SpecialEntries.Count == 0 || validLocations.Count == 0)
-        {
+        component.Center = spawnCenter;
+        component.Spawns = spawns;
+        component.Locations = validLocations.Select(c => c.Coordinates).OrderBy(c => (spawnCenter.Coordinates.Position - c.Position).Length()).Take(spawns.Count).ToList();
+    }
+    // Carpmosia-end - Vent critter fix
+
+    protected override void Started(EntityUid uid, VentCrittersRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    {
+        base.Started(uid, component, gameRule, args);
+
+        // Carpmosia-start - Vent critter fix
+        if (component.Center is null)
             return;
-        }
 
-        // guaranteed spawn
-        var specialEntry = RobustRandom.Pick(component.SpecialEntries);
-        var specialSpawn = RobustRandom.Pick(validLocations);
-        Spawn(specialEntry.PrototypeId, specialSpawn);
-
-        foreach (var location in validLocations)
-        {
-            foreach (var spawn in EntitySpawnCollection.GetSpawns(component.SpecialEntries, RobustRandom))
-            {
-                Spawn(spawn, location);
-            }
-        }
+        for (var i = 0; i < component.Spawns.Count; i++)
+            Spawn(component.Spawns[i], component.Locations[i % component.Locations.Count]);
+        // Carpmosia-end - Vent critter fix
     }
 }
