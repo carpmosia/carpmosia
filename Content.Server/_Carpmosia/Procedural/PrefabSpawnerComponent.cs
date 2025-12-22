b04c21098ea5b6cdc3a0f755a31805daecbd4ff0
Atakku <atakkudev@gmail.com>
2025-12-22 04:43:10 +0100

    fuck the circle, octagon is my new friend

---
+++ Content.Server/_Carpmosia/Procedural/PrefabSpawnerComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server.Procedural;
+
+/// <summary>
+/// Marker that fires Prefab's custom spanwning logic on MapInit
+/// </summary>
+[RegisterComponent]
+public sealed partial class PrefabSpawnerComponent : Component
+{
+}
