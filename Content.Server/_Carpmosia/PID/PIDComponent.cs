2b808233e2cbd432b6e52a201b4081e49b941ec9
korczoczek <danielkorczok@gmail.com>
2026-01-26 18:29:42 +0100

    move parameter definition to its own struct

--- Content.Server/_Carpmosia/PID/PIDComponent.cs
+++ Content.Server/_Carpmosia/PID/PIDComponent.cs
@@ -25,13 +25,13 @@ public struct PIDParams
     /// Accumulated integral
     /// </summary>
     [ViewVariables]
-    public float Integral;
+    public float Integral = 0f;
 
     /// <summary>
     /// Previous error value
     /// </summary>
     [ViewVariables]
-    public float PrevError;
+    public float PrevError = 0f;
 
     /// <summary>
     /// Upper controller output value constraint
@@ -44,4 +44,19 @@ public struct PIDParams
     /// </summary>
     [DataField]
     public float MinVal;
+
+    public PIDParams
+    (
+        float kp,
+        float ti,
+        float td,
+        float maxVal,
+        float minVal)
+    {
+        Kp = kp;
+        Ti = ti;
+        Td = td;
+        MaxVal = maxVal;
+        MinVal = minVal;
+    }
 }
