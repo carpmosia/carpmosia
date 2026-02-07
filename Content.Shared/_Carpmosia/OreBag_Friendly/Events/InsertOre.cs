bb5b9289982aa5b634f7a4932b5cd4dcc648b4d7
Jam07028 <joaopedrojesus85@gmail.com>
2026-02-07 16:43:04 -0400

    Learning "how to"

---
+++ Content.Shared/_Carpmosia/OreBag_Friendly/Events/InsertOre.cs
@@ -0,0 +1,6 @@
+using Content.Shared.InsertOre;
+using Robust.Shared.Serialization;
+
+namespace Content.OreBag_Friendly;
+[Serializable, NetSerializable]
+public sealed partial class InsertOreEvent : SimpleInsertOreEvent { }
