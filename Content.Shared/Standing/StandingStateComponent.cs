4b2ec5d9e6d0ca3f3b20f0e6081a1643c6b414f9
carprofiche <246941571+carprofiche@users.noreply.github.com>
2026-03-02 13:30:15 -0800

    add Incapacitated and LimpFrictionMod datafields to StandingStateComponent

--- Content.Shared/Standing/StandingStateComponent.cs
+++ Content.Shared/Standing/StandingStateComponent.cs
@@ -14,11 +14,20 @@ namespace Content.Shared.Standing
         [DataField, AutoNetworkedField]
         public bool Standing { get; set; } = true;
 
+        [DataField, AutoNetworkedField]
+        public bool Incapacitated { get; set; } = false;
+
         /// <summary>
         /// Friction modifier applied to an entity in the downed state.
         /// </summary>
         [DataField, AutoNetworkedField]
         public float DownFrictionMod = 0.4f;
+
+        /// <summary>
+        /// Friction modifier applied to an entity in an incapacitated (crit/dead) state.
+        /// </summary>
+        [DataField, AutoNetworkedField]
+        public float LimpFrictionMod = 3f;
 
         /// <summary>
         ///     List of fixtures that had their collision mask changed when the entity was downed.
