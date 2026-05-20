fedcf9b3cd8928803db4d94aaa1eb2cb85224ad5
Breadboxyy <lordbreadn@gmail.com>
2026-05-20 12:56:32 +0300

    I FUCKING HATE EORG KILL IT KILL IY

---
+++ Content.Client/_Carpmosia/RoundEnd/NoEorgPopupSystem.cs
@@ -0,0 +1,36 @@
+using Content.Shared.CCVar;
+using Content.Shared.GameTicking;
+using Robust.Shared.Configuration;
+
+namespace Content.Client._Carpmosia.RoundEnd;
+
+public sealed class NoEorgPopupSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    private NoEorgPopup? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeNetworkEvent<RoundEndMessageEvent>(OnRoundEnd);
+    }
+
+    private void OnRoundEnd(RoundEndMessageEvent ev)
+    {
+        if (_cfg.GetCVar(CCVars.CarpmosiaSkipRoundEndNoEorgPopup) || _cfg.GetCVar(CCVars.CarpmosiaRoundEndNoEorgPopup) == false)
+            return;
+
+        OpenNoEorgPopup();
+    }
+
+    private void OpenNoEorgPopup()
+    {
+        if (_window != null)
+            return;
+
+        _window = new NoEorgPopup();
+        _window.OpenCentered();
+        _window.OnClose += () => _window = null;
+    }
+}
--- Content.Shared/_Carpmosia/CCVars.cs
+++ Content.Shared/_Carpmosia/CCVars.cs
@@ -9,4 +9,23 @@ public sealed partial class CCVars
     /// </summary>
     public static readonly CVarDef<bool> AltEmotesMenu =
         CVarDef.Create("hud.alt_emotes_menu", false, CVar.CLIENTONLY | CVar.ARCHIVE);
+       /// <summary>
+    /// Whether the no EORG popup is enabled.
+    /// </summary>
+    public static readonly CVarDef<bool> CarpmosiaRoundEndNoEorgPopup =
+        CVarDef.Create("game.round_end_eorg_popup_enabled", true, CVar.SERVER | CVar.REPLICATED);
+
+    /// <summary>
+    /// Skip the no EORG popup.
+    /// </summary>
+    public static readonly CVarDef<bool> CarpmosiaSkipRoundEndNoEorgPopup =
+        CVarDef.Create("game.skip_round_end_eorg_popup", false, CVar.CLIENTONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// How long to display the EORG popup for.
+    /// </summary>
+    public static readonly CVarDef<float> CarpmosiaRoundEndNoEorgPopupTime =
+        CVarDef.Create("game.round_end_eorg_popup_time", 5f, CVar.SERVER | CVar.REPLICATED);
+
+ 
 }
