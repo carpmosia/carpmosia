074bb6c1b50f22c8bf1776599c1e2aeab5f9a64f
Atakku <atakkudev@gmail.com>
2026-07-19 14:23:15 +0200

    Revert to 49899efa36fc51233cd6a8c772b841df6d1bc719

--- Content.Shared/_Carpmosia/MedicalScanner/HealthAnalyzerPlusDoAfterEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Content.Shared.DoAfter;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared.MedicalScannerPlus;
-
-[Serializable, NetSerializable]
-public sealed partial class HealthAnalyzerPlusDoAfterEvent : SimpleDoAfterEvent
-{
-}
