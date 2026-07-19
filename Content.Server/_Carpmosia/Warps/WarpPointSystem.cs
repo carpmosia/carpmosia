71f337d29e11f780a35c221536ff42671574f3ac
Atakku <atakkudev@gmail.com>
2026-07-20 00:51:53 +0200

    remove unused code

--- Content.Server/_Carpmosia/Warps/WarpPointSystem.cs
+++ Content.Server/_Carpmosia/Warps/WarpPointSystem.cs
@@ -1,8 +1,6 @@
-using Content.Server.GameTicking;
 using Content.Server.Station.Components;
 using Content.Shared.Station.Components;
 using Content.Shared.Warps;
-using Robust.Shared.Map;
 
 namespace Content.Server.Warps;
 
@@ -36,34 +34,6 @@ public sealed class WarpPointSystem : EntitySystem
             if (string.IsNullOrEmpty(name) || name == "Map Entity")
                 return;
             ent.Comp.Origin = name;
-        }
-    }
-
-
-    private void OnRoundStart(GameRunLevelChangedEvent ev)
-    {
-        if (ev.New != GameRunLevel.InRound)
-            return;
-
-        var query = AllEntityQuery<WarpPointComponent>();
-        while (query.MoveNext(out var uid, out var comp))
-        {
-            if (string.IsNullOrEmpty(comp.Location))
-                continue;
-
-            if (Transform(uid).GridUid is not EntityUid grid)
-                continue;
-
-            if (TryComp<StationMemberComponent>(grid, out var member))
-            {
-                if (!TryComp<StationNameSetupComponent>(member.Station, out var name))
-                    continue;
-
-                comp.Location = name.ShortName + " - " + comp.Location;
-            }
-            // Fallback for misc maps (CentComm, Terminal, Arrivals)
-            else if (Transform(uid).MapUid is EntityUid map)
-                comp.Location = MetaData(map).EntityName + " - " + comp.Location;
         }
     }
 }
