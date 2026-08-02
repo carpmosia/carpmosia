6247c5f6b625cf3156cb315e730f06850cc79502
Atakku <atakkudev@gmail.com>
2026-08-02 22:24:16 +0200

    move

--- Content.Shared/_Carpmosia/CCVar/CCVars.cs
+++ Content.Shared/_Carpmosia/CCVar/CCVars.cs
@@ -27,28 +27,4 @@ public sealed partial class CCVars
     /// </summary>
     public static readonly CVarDef<bool> GameLobbyAutoVote =
         CVarDef.Create("game.lobby_auto_vote", false, CVar.SERVERONLY);
-
-    /// <summary>
-    ///     Enables HV cable docking
-    /// </summary>
-    public static readonly CVarDef<bool> DockCableHV =
-        CVarDef.Create("dock.cable_hv", true, CVar.SERVERONLY);
-
-    /// <summary>
-    ///     Enables MV cable docking
-    /// </summary>
-    public static readonly CVarDef<bool> DockCableMV =
-        CVarDef.Create("dock.cable_mv", false, CVar.SERVERONLY);
-
-    /// <summary>
-    ///     Enables LV cable docking
-    /// </summary>
-    public static readonly CVarDef<bool> DockCableLV =
-        CVarDef.Create("dock.cable_lv", false, CVar.SERVERONLY);
-
-    /// <summary>
-    ///     Enables pipe docking
-    /// </summary>
-    public static readonly CVarDef<bool> DockPipes =
-        CVarDef.Create("dock.pipes", true, CVar.SERVERONLY);
 }
