599fc2892a03fe460af1f14cccd146bf7e83bba3
Atakku <atakkudev@gmail.com>
2026-08-03 21:51:48 +0200

    testing shit

---
+++ Content.IntegrationTests/_Carpmosia/Tests/MappingGuidelineTest.cs
@@ -0,0 +1,112 @@
+#nullable enable
+using System.IO;
+using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Utility;
+using YamlDotNet.RepresentationModel;
+using Robust.Shared.ContentPack;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+using System.Linq;
+using Content.Shared.Wall;
+using Content.Shared.Light.Components;
+using System.Numerics;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
+using Content.Server.Atmos.Piping.Components;
+
+namespace Content.IntegrationTests.Tests;
+
+[TestFixture]
+public sealed partial class MappingGuidelineTest : GameTest
+{
+    private static readonly ResPath[] MapFiles = GameDataScrounger.FilesInDirectoryInVfs("/Maps/_Carpmosia", "amber.yml", false);
+
+    private static YamlNode LoadMapYaml(ResPath map, IResourceManager resMan)
+    {
+        var rootedPath = map.ToRootedPath();
+        if (!resMan.TryContentFileRead(rootedPath, out var fileStream))
+            Assert.Fail($"Map not found: {rootedPath}");
+
+        using var reader = new StreamReader(fileStream);
+        var yamlStream = new YamlStream();
+        yamlStream.Load(reader);
+
+        return yamlStream.Documents[0].RootNode;
+    }
+
+    private static (EntityUid, Vector2)? GetTilePos(YamlNode entNode)
+    {
+        var ent = (YamlMappingNode)entNode;
+
+        if (!ent.TryGetNode<YamlSequenceNode>("components", out var comps))
+            return null;
+
+        if (comps.First(x => x["type"].AsString() == "Transform") is not YamlMappingNode trans)
+            return null;
+
+        if (!trans.TryGetNode("parent", out var rawParent))
+            return null;
+
+        if (rawParent.ToString() == "invalid")
+            return null;
+
+        var parent = new EntityUid(rawParent.AsInt());
+
+        if (!trans.TryGetNode("pos", out var posRaw))
+            return null;
+
+        var rawPos = posRaw.AsString().Split(",").Select(float.Parse).ToArray();
+        var tilePos = new Vector2(rawPos[0], rawPos[1]).Floored();
+
+        return (parent, tilePos);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(MapFiles))]
+    public async Task MappedEntitiesUnderWallsTest(ResPath map)
+    {
+        var pair = Pair;
+        var server = pair.Server;
+
+        var resMan = server.ResolveDependency<IResourceManager>();
+        var protoMan = server.ResolveDependency<IPrototypeManager>();
+
+        var root = LoadMapYaml(map, resMan);
+        var entities = (YamlSequenceNode)root["entities"];
+
+        // Collect all walls
+        var wallProtos = Pair.GetPrototypesWithComponent<IsRoofComponent>().Select(x => x.Item1.ID).ToHashSet();
+
+        // Collect all wallmount entities
+        var wallmountProtos = Pair.GetPrototypesWithComponent<AtmosPipeColorComponent>().Select(x => x.Item1.ID).ToHashSet();
+
+        var wallPos = entities.Where(ent => wallProtos.Contains(ent["proto"].AsString()))
+            .SelectMany(x => ((YamlSequenceNode)x["entities"]).Select(GetTilePos).OfType<(EntityUid, Vector2)>())
+            .ToHashSet();
+
+        //Console.WriteLine($"{wallPos.Count} {wallPos}");
+
+        Assert.Multiple(() =>
+        {
+            foreach (var proto in entities)
+            {
+                var protoId = proto["proto"].AsString();
+
+                // Skip allowed entities
+                if (!wallmountProtos.Contains(protoId))
+                    continue;
+
+                foreach (var ent in (YamlSequenceNode)proto["entities"])
+                {
+                    // Skip invalid transforms
+                    if (GetTilePos(ent) is not { } trans)
+                        continue;
+
+                    Assert.That(wallPos,
+                        Does.Not.Contain(trans),
+                        $"\nMap {map} contains non-wallmount entitiy ({protoId}) mapped on wall ({trans})");
+                }
+            }
+        });
+    }
+}
