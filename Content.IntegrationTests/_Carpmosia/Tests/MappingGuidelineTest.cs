fa9fff9712f2be4b09db1339ada294c490ade296
Atakku <atakkudev@gmail.com>
2026-08-03 22:54:54 +0200

    cleanup

--- Content.IntegrationTests/_Carpmosia/Tests/MappingGuidelineTest.cs
+++ Content.IntegrationTests/_Carpmosia/Tests/MappingGuidelineTest.cs
@@ -12,14 +12,14 @@ using Content.Shared.Light.Components;
 using System.Numerics;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Maths;
-using Content.Server.Atmos.Piping.Components;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Content.IntegrationTests.Tests;
 
 [TestFixture]
 public sealed partial class MappingGuidelineTest : GameTest
 {
-    private static readonly ResPath[] MapFiles = GameDataScrounger.FilesInDirectoryInVfs("/Maps/_Carpmosia", "amber.yml", false);
+    private static readonly ResPath[] MapFiles = GameDataScrounger.FilesInDirectoryInVfs("/Maps/_Carpmosia", "*.yml", false);
 
     private static YamlNode? LoadMapYaml(ResPath map, IResourceManager resMan)
     {
@@ -66,6 +66,7 @@ public sealed partial class MappingGuidelineTest : GameTest
 
     [Test]
     [TestCaseSource(nameof(MapFiles))]
+    [SuppressMessage("Assertion", "NUnit2014:Use SomeItemsConstraint for better assertion messages in case of failure")]
     public async Task MappedEntitiesUnderWallsTest(ResPath map)
     {
         var pair = Pair;
@@ -83,7 +84,7 @@ public sealed partial class MappingGuidelineTest : GameTest
         var wallProtos = Pair.GetPrototypesWithComponent<IsRoofComponent>().Select(x => x.Item1.ID).ToHashSet();
 
         // Collect all wallmount entities
-        var wallmountProtos = Pair.GetPrototypesWithComponent<AtmosPipeColorComponent>().Select(x => x.Item1.ID).ToHashSet();
+        var wallmountProtos = Pair.GetPrototypesWithComponent<WallMountComponent>().Select(x => x.Item1.ID).ToHashSet();
 
         var wallPos = entities.Where(ent => wallProtos.Contains(ent["proto"].AsString()))
             .SelectMany(x => ((YamlSequenceNode)x["entities"]).Select(GetTilePos).OfType<(EntityUid, Vector2)>())
@@ -98,7 +99,7 @@ public sealed partial class MappingGuidelineTest : GameTest
                 var protoId = proto["proto"].AsString();
 
                 // Skip allowed entities
-                if (!wallmountProtos.Contains(protoId))
+                if (wallmountProtos.Contains(protoId))
                     continue;
 
                 foreach (var ent in (YamlSequenceNode)proto["entities"])
@@ -107,9 +108,8 @@ public sealed partial class MappingGuidelineTest : GameTest
                     if (GetTilePos(ent) is not { } trans)
                         continue;
 
-                    Assert.That(wallPos,
-                        Does.Not.Contain(trans),
-                        $"\nMap {map} contains non-wallmount entitiy ({protoId}) mapped on wall ({trans})");
+                    Assert.That(!wallPos.Contains(trans),
+                        $"\nMap {map} contains non-wallmount entity {protoId} ({ent["uid"]}) mapped under a wall ({trans})");
                 }
             }
         });
