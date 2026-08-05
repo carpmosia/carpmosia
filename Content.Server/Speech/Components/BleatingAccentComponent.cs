f351cad780212ce1e634bca5fde718e64a9dc310
carprofiche <246941571+carprofiche@users.noreply.github.com>
2026-08-05 05:56:01 -0700

    baa

--- Content.Server/Speech/Components/BleatingAccentComponent.cs
+++ Content.Server/Speech/Components/BleatingAccentComponent.cs
@@ -6,4 +6,18 @@ namespace Content.Server.Speech.Components;
 /// Makes this entity speak like a sheep or a goat in all chat messages it sends.
 /// </summary>
 [RegisterComponent]
-public sealed partial class BleatingAccentComponent : BaseAccentComponent;
+public sealed partial class BleatingAccentComponent : BaseAccentComponent
+{
+    /// <summary>
+    /// Amount of vowel repeats, baaaah.
+    /// </summary>
+    [DataField]
+    public int Repeats = 4;
+
+    /// <summary>
+    /// If a dash will be inserted in the middle of repeated vowels.
+    /// Rounds up (baa-ah rather than ba-aah).
+    /// </summary>
+    [DataField]
+    public bool Wobble;
+}
--- Content.Server/Speech/EntitySystems/BleatingAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/BleatingAccentSystem.cs
@@ -1,17 +1,27 @@
+using System.Linq;
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
 using Content.Shared.Speech.EntitySystems;
+using Robust.Shared.Random;
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed partial class BleatingAccentSystem : RelayAccentSystem<BleatingAccentComponent>
 {
-    private static readonly Regex BleatRegex = new("([mbdlpwhrkcnytfo])([aiu])", RegexOptions.IgnoreCase);
+    [Dependency] private IRobustRandom _random = default!;
+
+    [GeneratedRegex("([mbdlpwhrkcnytfo])([aiu])", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex BleatRegex();
 
     public override string Accentuate(string message, Entity<BleatingAccentComponent>? ent = null)
     {
-        // Repeats the vowel in certain consonant-vowel pairs
-        // So you taaaalk liiiike thiiiis
-        return BleatRegex.Replace(message, "$1$2$2$2$2");
+        // Repeats the vowel in certain consonant-vowel pairs and adds a dash in the middle (if set).
+        // So you taa-aalk lii-iike thii-iis
+        var repeatAmount = ent.HasValue ? ent.Value.Comp.Repeats : 3;
+        var replacement = "$1"
+                          + string.Concat(Enumerable.Repeat("$2", (repeatAmount / 2) + 1))
+                          + (ent.HasValue && ent.Value.Comp.Wobble ? "-" : "")
+                          + string.Concat(Enumerable.Repeat("$2", repeatAmount - ((repeatAmount / 2) + 1)));
+        return BleatRegex().Replace(message, replacement);
     }
 }
