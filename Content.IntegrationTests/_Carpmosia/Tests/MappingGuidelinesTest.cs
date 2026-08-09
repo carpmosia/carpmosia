81126d4afaa784b5b7623af46209fbc545a792e7
Atakku <atakkudev@gmail.com>
2026-08-09 22:46:15 +0200

    meow

--- Content.IntegrationTests/_Carpmosia/Tests/MappingGuidelinesTest.cs
+++ Content.IntegrationTests/_Carpmosia/Tests/MappingGuidelinesTest.cs
@@ -31,7 +31,8 @@ public sealed partial class MappingGuidelinesTest : GameTest
         // new("/Maps/_Carpmosia/oasis.yml"),
         // new("/Maps/_Carpmosia/packed.yml"),
         // new("/Maps/_Carpmosia/saltern.yml"),
-        // new("/Maps/_Carpmosia/sparks.yml")
+        // new("/Maps/_Carpmosia/sparks.yml"),
+        new("/Maps/_Carpmosia/box.yml")
     ];
     //private static readonly ResPath[] AllMapFiles = [.. GameDataScrounger.FilesInDirectoryInVfs("/Maps/_Carpmosia", "*.yml", true).Where(x => !x.ToString().StartsWith("/Maps/_Carpmosia/Legacy/"))];
     //private static readonly ResPath[] StationMaps = [.. GameDataScrounger.FilesInDirectoryInVfs("/Maps/_Carpmosia", "*.yml", false).Where(x => !x.ToString().StartsWith("/Maps/_Carpmosia/centcomm.yml"))];
