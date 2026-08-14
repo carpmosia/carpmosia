92ad3ca2dd4e15c0b7cec8aa1ca3eb89fcdebdf1
Atakku <atakkudev@gmail.com>
2026-08-14 13:44:44 +0200

    unused import

--- Content.Client/RoundEnd/RoundEndSummaryWindow.cs
+++ Content.Client/RoundEnd/RoundEndSummaryWindow.cs
@@ -7,7 +7,6 @@ using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Shared.Utility;
 using static Robust.Client.UserInterface.Controls.BoxContainer;
-using Content.Client.Stylesheets;
 
 namespace Content.Client.RoundEnd;
 
