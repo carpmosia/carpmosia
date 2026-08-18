3b872715598d9925ef7dba39842dcfd67662b530
Atakku <atakkudev@gmail.com>
2026-08-18 18:13:12 +0200

    fuh

--- Content.IntegrationTests/_Carpmosia/Tests/MapRulesTest.cs
+++ Content.IntegrationTests/_Carpmosia/Tests/MapRulesTest.cs
@@ -29,7 +29,6 @@ public sealed partial class MapRulesTest : GameTest
        // Maps pending fixes
        "/Maps/_Carpmosia/feint.yml",
        "/Maps/_Carpmosia/lampocteis.yml",
-       "/Maps/_Carpmosia/oasis.yml",
        "/Maps/_Carpmosia/packed.yml",
        "/Maps/_Carpmosia/saltern.yml",
        "/Maps/_Carpmosia/sparks.yml",
