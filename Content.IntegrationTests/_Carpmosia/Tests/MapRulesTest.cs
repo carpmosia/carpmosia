95753ee16df51beeb1cce3792c68c9312573a434
Atakku <atakkudev@gmail.com>
2026-08-18 18:14:19 +0200

    Re-Carpening of Sparks

--- Content.IntegrationTests/_Carpmosia/Tests/MapRulesTest.cs
+++ Content.IntegrationTests/_Carpmosia/Tests/MapRulesTest.cs
@@ -32,7 +32,6 @@ public sealed partial class MapRulesTest : GameTest
        "/Maps/_Carpmosia/oasis.yml",
        "/Maps/_Carpmosia/packed.yml",
        "/Maps/_Carpmosia/saltern.yml",
-       "/Maps/_Carpmosia/sparks.yml",
        // Shuttles gonna be fixed last
        "/Maps/_Carpmosia/Shuttles/",
     ];
