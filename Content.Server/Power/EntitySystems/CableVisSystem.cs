281e13b210bf44cbb35ca851ec4d5d80cb1feee5
Atakku <atakkudev@gmail.com>
2026-08-20 15:48:04 +0200

    Fixed connection visuals

--- Content.Server/Power/EntitySystems/CableVisSystem.cs
+++ Content.Server/Power/EntitySystems/CableVisSystem.cs
@@ -36,7 +36,7 @@ namespace Content.Server.Power.EntitySystems
 
             foreach (var reachable in node.ReachableNodes)
             {
-                if (reachable is not CableNode)
+                if (reachable is not CableNode && reachable is not CableDeviceNodeAdjacent) // Carpmosia-edit - Wallmount adjacent power
                     continue;
 
                 var otherTransform = Transform(reachable.Owner);
