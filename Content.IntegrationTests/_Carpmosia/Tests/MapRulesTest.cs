9f4e988d9aafaf35c292a735244850cc7ff4691d
Atakku <atakkudev@gmail.com>
2026-09-03 13:08:46 +0200

    Fixed up crimson.yml

--- Content.IntegrationTests/_Carpmosia/Tests/MapRulesTest.cs
+++ Content.IntegrationTests/_Carpmosia/Tests/MapRulesTest.cs
@@ -30,7 +30,6 @@ public sealed partial class MapRulesTest : GameTest
        "/Maps/_Carpmosia/Shuttles/Emergency/accordia.yml",
        "/Maps/_Carpmosia/Shuttles/Emergency/brick.yml",
        "/Maps/_Carpmosia/Shuttles/Emergency/compact.yml",
-       "/Maps/_Carpmosia/Shuttles/Emergency/crimson.yml",
        "/Maps/_Carpmosia/Shuttles/Event/ert_mixed.yml",
     ];
 
